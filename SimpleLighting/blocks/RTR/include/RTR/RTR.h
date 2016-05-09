dcbece79b198c7c1b5b3a2bf8ab5deae18bd4ab1
Jan <s65613@beuth-hochschule.de>
2016-05-09 21:32:51 +0200

    deleted project

--- SimpleLighting/blocks/RTR/include/RTR/RTR.h
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright 2016 Henrik Tramberend, Hartmut Schirmacher
-//
-
-#pragma once
-
-#include "RTR/ObjLoader.hpp"
-#include "RTR/SceneGraph.hpp"
-#include "RTR/WatchThis.hpp"
--- SimpleLighting/blocks/RTR/src/RTR/tiny_obj_loader.cc
+++
@@ -1,2 +0,0 @@
-#define TINYOBJLOADER_IMPLEMENTATION
-#include "RTR/tiny_obj_loader.h"
